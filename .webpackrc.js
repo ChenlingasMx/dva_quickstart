0e120a03da15664baf55c4b709c2911000dfd4d8
WX <42308734@qq.com>
2021-06-11 14:35:08 +0800

    fix:页面布局

---
+++ .webpackrc.js
@@ -0,0 +1,9 @@
+export default{
+    "extraBabelPlugins": [
+        ["import", { "libraryName": "antd", "libraryDirectory": "es", "style": true }]
+    ],
+    "theme": {
+    	// 具体配置看antd官网文档了
+        "@primary-color": "#F18700"
+    },
+}
--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,13 +1,56 @@
 import React from 'react';
 import { connect } from 'dva';
-import styles from './IndexPage.css';
-import { Table, Radio, Card } from 'antd'
+import styles from './IndexPage.less';
+import { Table, Radio, Card, Statistic } from 'antd'
+import { ArrowUpOutlined, ArrowDownOutlined } from '@ant-design/icons';
 import { columns } from './columns'
 
 function IndexPage() {
   const dataSource = [
     {
-      key: '1',
+      id: '1',
+      name: '湖北',
+      age: '合计',
+      address: 213,
+      max: 0.17,
+      tom: 98,
+      add: 84.9,
+      tongbi: -2,
+      tongbilv: -1.1
+    }, {
+      id: '2',
+      name: '湖北',
+      age: '合计',
+      address: 213,
+      max: 0.17,
+      tom: 98,
+      add: 84.9,
+      tongbi: -2,
+      tongbilv: -1.1
+    }
+    , {
+      id: '3',
+      name: '湖北',
+      age: '合计',
+      address: 213,
+      max: 0.17,
+      tom: 98,
+      add: 84.9,
+      tongbi: -2,
+      tongbilv: -1.1
+    }, {
+      id: '4',
+      name: '湖北',
+      age: '合计',
+      address: 213,
+      max: 0.17,
+      tom: 98,
+      add: 84.9,
+      tongbi: -2,
+      tongbilv: -1.1
+    },
+    {
+      id: '5',
       name: '湖北',
       age: '合计',
       address: 213,
@@ -20,6 +63,7 @@ function IndexPage() {
   ];
   return (
     <div className={styles.normal}>
+      {/* 头部 */}
       <div className={styles.title}>
         <div>
           <span>安能快运05月27日</span>
@@ -30,34 +74,53 @@ function IndexPage() {
           <span style={{ marginLeft: '0.3em' }}>12:00:00</span>
         </div>
       </div>
+      {/* 卡片 */}
       <div className={styles.button_tab}>
-        <Radio.Group size='small' buttonStyle="outline" >
-          <Radio.Button value="small">全网</Radio.Button>
-          <Radio.Button value="small">网络</Radio.Button>
-          <Radio.Button value="small">市场</Radio.Button>
+        <Radio.Group size='small' value="1" buttonStyle="solid">
+          <Radio.Button value="1">全网</Radio.Button>
+          <Radio.Button value="2">网络</Radio.Button>
+          <Radio.Button value="3">市场</Radio.Button>
         </Radio.Group>
-        <Radio.Group size='small' buttonStyle="outline" >
-          <Radio.Button value="small">查看大区</Radio.Button>
-          <Radio.Button value="small">查看分拨</Radio.Button>
-          <Radio.Button value="small">查看片区</Radio.Button>
+        <Radio.Group size='small' buttonStyle="outline" value="1" >
+          <Radio.Button value="1">查看大区</Radio.Button>
+          <Radio.Button value="2">查看分拨</Radio.Button>
+          <Radio.Button value="3">查看片区</Radio.Button>
         </Radio.Group>
       </div>
-      <div style={{ display: 'flex', justifyContent: "center", alignContent: "center", flexDirection: "row", padding: '0.5em' }}>
-        <Card size="small" style={{ width: 300, marginRight: '0.3em' }}>
-          <p>当前货量</p>
-          <h3>513</h3>
+      <div style={{ display: 'flex', justifyContent: 'space-around', alignContent: "center", flexDirection: "row", padding: '0.5em' }}>
+        <Card size="small" title="当前货量" headStyle={{ color: "#ccc" }} style={{ marginRight: '0.3em' }}>
+          <Statistic
+            value={11.28}
+            precision={2}
+            valueStyle={{ color: '#cf1322', fontSize: 20 }}
+            prefix={<ArrowUpOutlined />}
+            suffix="%"
+          />
         </Card>
-        <Card size="small" style={{ width: 300, marginRight: '0.3em' }}>
-          <p>当前货量</p>
-          <h3>513</h3>
+        <Card size="small" title="当前货量百分比" headStyle={{ color: "#ccc" }} style={{ marginRight: '0.3em' }}>
+          <Statistic
+            value={11.28}
+            precision={2}
+            valueStyle={{ color: '#3f8600', fontSize: 20 }}
+            prefix={<ArrowDownOutlined />}
+            suffix="t"
+          />
         </Card>
-        <Card size="small" style={{ width: 300 }}>
-          <p>当前货量</p>
-          <h3>513</h3>
+        <Card size="small" title="当前货量" headStyle={{ color: "#ccc" }}>
+          <Statistic
+            value={30.28}
+            precision={2}
+            valueStyle={{ color: '#cf1322', fontSize: 20 }}
+            suffix="kg"
+            prefix={<ArrowUpOutlined />}
+          />
         </Card>
       </div>
+      {/* Table */}
       <div style={{ padding: '0.5em' }}>
         <Table
+          className={styles.table_row}
+          id="id"
           dataSource={dataSource}
           columns={columns}
           scroll={{ x: 500, y: '100%' }}
